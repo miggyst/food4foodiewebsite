161fca4649a235b9060a23fe8a007ce231adbd42
Miggy <atankia@headstorm.com>
2021-11-16 09:04:57 -0600

    Update git actions to invalidate on ci.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to food4foodie
+          Welcome to food4foodie where all your food desires come true
         </p>
         <a
           className="App-link"
